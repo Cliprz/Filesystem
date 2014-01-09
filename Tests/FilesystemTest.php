bf6e5292a0590d332ab3908120ee0b041ed2fafb
Yousef Ismaeil <Cliprz@gmail.com>
2014-01-09 11:30:27 +0300

    Full PHPUnit test

--- Tests/FilesystemTest.php
+++ Tests/FilesystemTest.php
@@ -1,10 +1,11 @@
-<?php namespace Cliprz\Filesystem\Filesystem\Tests;
+<?php
 
-$classPath = realpath('../Filesystem.php');
-
-// Call composer to get PHPUnit or you can call your PHPUnit autoloader
-include ('../vendor/autoload.php');
-include($classPath);
+/**
+ * This Test need PHPUnit
+ * Call composer autoload to get PHPUnit or Call PHPUnit autoloader
+ * @note Don't forget to require cliprz/filesystem on composer
+ */
+include('../vendor/autoload.php');
 
 use Cliprz\Filesystem\Filesystem as Filesystem;
 use PHPUnit_Framework_TestCase as TestCase;
@@ -28,6 +29,87 @@ class FilesystemTest extends TestCase {
 	public function testExtension () {
 		$this->assertEquals('php',$this->Filesystem->extension(realpath('../Filesystem.php')));
 		$this->assertEquals('json',$this->Filesystem->extension(realpath('../composer.json')));
+	}
+
+	public function testGuessMimeType () {
+		$this->assertEquals('application/zip',$this->Filesystem->guessMimeType('zip'));
+	}
+
+	public function testProp () {
+		var_dump($this->Filesystem->prop($this->thisPath.'FilesystemTest.php'));
+	}
+
+	public function testDelete () {
+		$TestingDeleteDirectory = $this->thisPath.'TestingDeleteDirectory';
+		if (!is_dir($TestingDeleteDirectory)) {
+			mkdir($TestingDeleteDirectory,0777,true);
+		}
+
+		$TestingDeleteFile = $this->thisPath.'TestingDeleteFile.php';
+		if (!is_file($TestingDeleteFile)) {
+			file_put_contents($TestingDeleteFile,'');
+		}
+
+		$this->assertTrue($this->Filesystem->delete($TestingDeleteDirectory));
+		$this->assertTrue($this->Filesystem->delete($TestingDeleteFile));
+	}
+
+	public function testFormat () {
+		$TestingFormatDirectory = $this->thisPath.'TestingFormatDirectory';
+
+		$files = [
+			'Abc.php',
+			'Xyz.php',
+			'Foo.php',
+			'Bar.php'
+		];
+
+		if (!is_dir($TestingFormatDirectory)) {
+			mkdir($TestingFormatDirectory,0777,true);
+			foreach ($files as $f) {
+				if (!is_file($TestingFormatDirectory.DIRECTORY_SEPARATOR.$f)) {
+					file_put_contents($TestingFormatDirectory.DIRECTORY_SEPARATOR.$f,'');
+				}
+			}
+		}
+
+		$this->assertTrue($this->Filesystem->format($TestingFormatDirectory));		
+	}
+
+	public function testCopyDirectory () {
+		$TestingCopyDirectory = $this->thisPath.'TestingCopyDirectory';
+
+		$files = [
+			'Abc.php',
+			'Xyz.php',
+			'Foo.php',
+			'Bar.php'
+		];
+
+		if (!is_dir($TestingCopyDirectory)) {
+			mkdir($TestingCopyDirectory,0777,true);
+			foreach ($files as $f) {
+				if (!is_file($TestingCopyDirectory.DIRECTORY_SEPARATOR.$f)) {
+					file_put_contents($TestingCopyDirectory.DIRECTORY_SEPARATOR.$f,'');
+				}
+			}
+		}
+
+		$this->assertTrue($this->Filesystem->copyDirectory(
+			$TestingCopyDirectory,
+			$this->thisPath.'TestingDestCopyDirectory'));
+
+		$this->assertTrue($this->Filesystem->format($TestingCopyDirectory));
+		$this->assertTrue($this->Filesystem->format($this->thisPath.'TestingDestCopyDirectory'));
+	}
+
+	public function testBytesToSize () {
+		var_dump($this->Filesystem->bytesToSize(139230));
+	}
+
+	public function testDirectorySize () {
+		$size = $this->Filesystem->directorySize(realpath('../'));
+		var_dump($size,$this->Filesystem->bytesToSize($size));
 	}
 
 }
